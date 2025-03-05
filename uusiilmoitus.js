792bf578cb288fc7907fc43d83a81229785941f3
Valo Riutta <valo.riutta@gmail.com>
2025-03-05 11:45:53 +0200

    lisäys

--- uusiilmoitus.js
+++ uusiilmoitus.js
@@ -10,7 +10,8 @@ function lisaaIlmoitus(){
     let sijainti = document.getElementById('sijainti').value
     let kuvaus = document.getElementById('kuvaus').value
     let hinta = document.getElementById('hinta').value
-    let uusiIlmoitus = {'nimi': nimi, 'sijainti': sijainti, 'kuvaus': kuvaus, 'hinta': hinta}
+    let luoja = JSON.parse(localStorage.getItem('kirjautunutkayttaja'))
+    let uusiIlmoitus = {'luoja': luoja, 'nimi': nimi, 'sijainti': sijainti, 'kuvaus': kuvaus, 'hinta': hinta}
     ilmoitukset.push(uusiIlmoitus)
     localStorage.setItem('ilmoitukset', JSON.stringify(ilmoitukset))
     location.replace('./kirppis.html')
