7bc947db8ac7c184a3c700a0ba56c85e6fc47371
Valo Riutta <valo.riutta@gmail.com>
2025-03-27 14:04:08 +0200

    korjaus

--- uusiilmoitus.js
+++ uusiilmoitus.js
@@ -2,49 +2,74 @@ let ilmoitukset;
 var MyApp = {};
 
 function upload() {
-    const fileUploadInput = document.querySelector('.file-uploader');
-    const image = fileUploadInput.files[0];
+    try{
+        const fileUploadInput = document.querySelector('.file-uploader');
+        const image = fileUploadInput.files[0];
 
-    if (!image.type.includes('image')) {
-        return alert('Only images are allowed!');
-    }
-
-    if (image.size > 10_000_000) {
-        return alert('Maximum upload size is 10MB!');
-    }
-
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(image);
-
-    fileReader.onload = (fileReaderEvent) => {
-        let base64String = fileReaderEvent.target.result;
-
-        if (base64String.startsWith("data:image/")) {
-            MyApp.tuotekuva = base64String;
-        } else {
-            console.error("Image data is not in the correct format!");
+        if (!image.type.includes('image')) {
+            return alert('Only images are allowed!');
         }
-    };
+
+        if (image.size > 10_000_000) {
+            return alert('Maximum upload size is 10MB!');
+        }
+
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(image);
+
+        fileReader.onload = (fileReaderEvent) => {
+            let base64String = fileReaderEvent.target.result;
+
+            if (base64String.startsWith("data:image/")) {
+                MyApp.tuotekuva = base64String;
+            } else {
+                MyApp.tuotekuva = './lataus.png'
+            }
+        };
+    }
+    catch(err){
+        MyApp.tuotekuva = './lataus.png'
+    }
 }
 
 
 function lisaaIlmoitus(){
-    if (localStorage.getItem('ilmoitukset')) {
-        ilmoitukset = JSON.parse(localStorage.getItem('ilmoitukset'));
-    } else {
-        ilmoitukset = [];
+    try{
+        if (localStorage.getItem('ilmoitukset')) {
+            ilmoitukset = JSON.parse(localStorage.getItem('ilmoitukset'));
+        } else {
+            ilmoitukset = [];
+        }
+        let nimi = document.getElementById('nimi').value
+        let sijainti = document.getElementById('sijainti').value
+        let kuvaus = document.getElementById('kuvaus').value
+        let hinta = document.getElementById('hinta').value
+        let luoja = localStorage.getItem('kirjautunutkayttaja')
+        let kategoria = document.getElementById('kategoria').value
+        let kuva = MyApp.tuotekuva
+        let uusiIlmoitus = {'luoja': luoja, 'nimi': nimi, 'sijainti': sijainti, 'kuvaus': kuvaus, 'hinta': hinta, 'kategoria': kategoria, 'kuva': kuva}
+        ilmoitukset.push(uusiIlmoitus)
+        localStorage.setItem('ilmoitukset', JSON.stringify(ilmoitukset))
+        location.replace('./kirppis.html')
     }
-    let nimi = document.getElementById('nimi').value
-    let sijainti = document.getElementById('sijainti').value
-    let kuvaus = document.getElementById('kuvaus').value
-    let hinta = document.getElementById('hinta').value
-    let luoja = localStorage.getItem('kirjautunutkayttaja')
-    let kategoria = document.getElementById('kategoria').value
-    let kuva = MyApp.tuotekuva
-    let uusiIlmoitus = {'luoja': luoja, 'nimi': nimi, 'sijainti': sijainti, 'kuvaus': kuvaus, 'hinta': hinta, 'kategoria': kategoria, 'kuva': kuva}
-    ilmoitukset.push(uusiIlmoitus)
-    localStorage.setItem('ilmoitukset', JSON.stringify(ilmoitukset))
-    location.replace('./kirppis.html')
+    catch(err){
+        if (localStorage.getItem('ilmoitukset')) {
+            ilmoitukset = JSON.parse(localStorage.getItem('ilmoitukset'));
+        } else {
+            ilmoitukset = [];
+        }
+        let nimi = document.getElementById('nimi').value
+        let sijainti = document.getElementById('sijainti').value
+        let kuvaus = document.getElementById('kuvaus').value
+        let hinta = document.getElementById('hinta').value
+        let luoja = localStorage.getItem('kirjautunutkayttaja')
+        let kategoria = document.getElementById('kategoria').value
+        let kuva = './lataus.png'
+        let uusiIlmoitus = {'luoja': luoja, 'nimi': nimi, 'sijainti': sijainti, 'kuvaus': kuvaus, 'hinta': hinta, 'kategoria': kategoria, 'kuva': kuva}
+        ilmoitukset.push(uusiIlmoitus)
+        localStorage.setItem('ilmoitukset', JSON.stringify(ilmoitukset))
+        location.replace('./kirppis.html')
+    }
 }
 
 function peruuta(){
